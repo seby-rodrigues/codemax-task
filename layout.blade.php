0a44580254985959a7759e22595caa6e3192853f
seby-rodrigues <81638294+seby-rodrigues@users.noreply.github.com>
2021-08-26 20:00:01 +0530

    Add files via upload

---
+++ layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel 8 CRUD App</title>
+    <link rel="shortcut icon" href="#">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+  
+<div class="container">
+    @yield('content')
+</div>
+   
+</body>
+</html>
